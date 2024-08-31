f9c927d9e30639f1f73506152b2ba3776d6130ed
Farooquekk <22sw040@students.muet.edu.pk>
2024-08-31 18:06:40 +0500

    Add files via upload

---
+++ SQL Programs/GPA_ViewCreation.sql.sql
@@ -0,0 +1,26 @@
+/*CREATE TABLE Student (
+    StudentID INT PRIMARY KEY ,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    gpa INT,
+    
+); */
+
+
+/*INSERT INTO Student (StudentID, FirstName, LastName, GPA) VALUES
+(37, 'Talha', 'Shafique', 3.3),
+(31, 'Rohal', 'Jamal', 3.6),
+(79, 'Magan', 'Das', 4),
+(137, 'Muhammad', 'Rafy', 3),
+(22, 'Abid', 'Ali', 4);*/
+
+
+
+
+--select * from Student;
+
+
+--create VIEW myView as 
+--select * from Student where gpa > 3.5;
+select * from myView; 
+
